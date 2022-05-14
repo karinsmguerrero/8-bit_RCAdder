c7f8149f01ace742b6e6642317a0c0f820ba5300
Karina <karinamguerrero@estudiantec.cr>
2022-05-13 21:06:56 -0600

    Calculate error distance

--- python/main.py
+++ python/main.py
@@ -1,26 +1,74 @@
-def readFile(fileToCompare):
-    aproximateLines = []
-    with open("/home/karina/Verilog/Full_adder/python/files/" + fileToCompare) as fp:
+from collections import Counter
+
+def readFile(file):
+    lines = []
+    with open("/home/karina/Verilog/Full_adder/python/" + file) as fp:
         for line in fp:
-            aproximateLines.append(line.strip())
-    return aproximateLines
-    #print(lines)
+            lines.append(line.strip())
+    return lines
+
+def readFileAsList(file):
+    with open("/home/karina/Verilog/Full_adder/python/" + file) as fp:
+        content = fp.read()
+        content_list = content.split(",")
+        lenght = len(content_list)
+        lines = content_list[1:lenght -1]
+        res = []
+        for line in lines:
+            res.append(line.strip())
+
+        #print(content_list[1:lenght -1])
+    return  res
+
+def generatePMF(name):
+    exactLines = readFile('/files/output_RippleCarryAdder.txt')
+    aproximateLines = readFile('/files/output_' + name + '.txt')
+    errorDistance = [] #error distance
+    for index, line in enumerate(exactLines):
+        errorDistance.append(abs(int(line) - int(aproximateLines[index])))
+    # writing to file
+    with open('/home/karina/Verilog/Full_adder/python/results/PMF_' + name + '.txt', 'w') as fp:
+        fp.writelines(str(errorDistance))
+
+def getErrorDistances(name):
+    results = readFileAsList('/results/PMF_' + name + '.txt')
+    uniques = set(results)
+    print(uniques)
+    for index, item in enumerate(uniques):
+        counts = Counter(results)
+        print(str(item) + '=' + str(counts[item]))
+    
+    #print(len(results))
+    #return set(results)
 
 def main():
-    exactLines = []
-    with open("/home/karina/Verilog/Full_adder/python/files/output_RippleCarryAdder.txt") as fp:
-        for line in fp:
-            exactLines.append(line.strip())
+    """
+    generatePMF('RippleCarryAdder')
+    generatePMF('LSB_One_AproximateRCAdder')
+    generatePMF('LSB_Two_AproximateRCAdder')
+    generatePMF('LSB_Three_AproximateRCAdder')
+    generatePMF('LSB_Four_AproximateRCAdder')
+    generatePMF('LSB_One_AproximateORAdder')
+    generatePMF('LSB_Two_AproximateORAdder')
+    generatePMF('LSB_Three_AproximateORAdder')
+    generatePMF('LSB_Four_AproximateORAdder')
+    """
 
-    aproximateLines = readFile("output_LSB_One_AproximateRCAdder.txt")
 
-    probability = []
-    for index, line in enumerate(exactLines):
-        probability.append(int(line) - int(aproximateLines[index]))
-    # writing to file
-    file1 = open('/home/karina/Verilog/Full_adder/python/results/result_LSB_One_AproximateRCAdder.txt', 'w')
-    file1.writelines(str(probability))
-    file1.close()
+    getErrorDistances('LSB_Three_AproximateRCAdder')
+
+    """
+    getErrorDistances('RippleCarryAdder')
+    getErrorDistances('LSB_One_AproximateRCAdder')
+    getErrorDistances('LSB_Two_AproximateRCAdder')
+    getErrorDistances('LSB_Three_AproximateRCAdder')
+    getErrorDistances('LSB_Four_AproximateRCAdder')
+    getErrorDistances('LSB_One_AproximateORAdder')
+    getErrorDistances('LSB_Two_AproximateORAdder')
+    getErrorDistances('LSB_Three_AproximateORAdder')
+    getErrorDistances('LSB_Four_AproximateORAdder')
+    """
+
 
 if __name__ == "__main__":
     main()
